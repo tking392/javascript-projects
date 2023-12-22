d700a42214b18aebd1ed12ef87d3d3ea264b8a23
Travis <azai392@gmail.com>
2023-12-21 22:45:28 -0600

    Messing around with \n and \t

--- how-to-write-code/consolelogexamples01.js
+++ how-to-write-code/consolelogexamples01.js
@@ -1,6 +1,6 @@
 console.log('Hello, JavaScript.');
 console.log(2001);
-console.log("What","do","commas","do?");
+console.log("What\tdo\ttcommas\ndo?");
 console.log("Does", "adding",      "space", "matter?");
 console.log('Launch' + 'Code');
 console.log("LaunchCode was founded in", 2013);

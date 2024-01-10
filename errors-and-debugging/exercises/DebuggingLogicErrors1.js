18f2cb73e10c428aa19eb812bb3a0e214ee4c4d4
Travis <azai392@gmail.com>
2024-01-10 00:48:39 -0600

    done

--- errors-and-debugging/exercises/DebuggingLogicErrors1.js
+++ errors-and-debugging/exercises/DebuggingLogicErrors1.js
@@ -1,6 +1,6 @@
 // Run this sample code as-is and examine the output. 
-// Should the shuttle have launched? 
-// Did it?
+// Should the shuttle have launched? - No it should not have launched. 
+// Did it? - Yes, it did 
 // Do not worry about fixing the code yet, we will do that in the next series of exercises.
 
 let launchReady = false;

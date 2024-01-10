4b13f855a6c3ee48e003842e9458f1e0eae88b60
Travis <azai392@gmail.com>
2024-01-10 00:55:36 -0600

    done

--- errors-and-debugging/exercises/DebuggingLogicErrors3.js
+++ errors-and-debugging/exercises/DebuggingLogicErrors3.js
@@ -3,7 +3,7 @@
 // Add another console.log(launchReady) after this block and run the program.
 
 // Given the values for crewStatus and computerStatus, should launchReady be true or false after the check?  
-// Is the program behaving as expected?
+// Is the program behaving as expected? - Yes it is still acting as expected. 
 
 let launchReady = false;
 // let fuelLevel = 17000;
@@ -25,6 +25,8 @@ if (crewStatus && computerStatus === 'green'){
    console.log('WARNING: Crew or computer not ready!');
    launchReady = false;
 }
+
+console.log(launchReady);
 
 // if (launchReady) {
 //    console.log('10, 9, 8, 7, 6, 5, 4, 3, 2, 1...');

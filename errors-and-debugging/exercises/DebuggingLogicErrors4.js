64a7196417921b205b8782e8bd878b33a0103faf
Travis <azai392@gmail.com>
2024-01-10 01:08:36 -0600

    done

--- errors-and-debugging/exercises/DebuggingLogicErrors4.js
+++ errors-and-debugging/exercises/DebuggingLogicErrors4.js
@@ -1,8 +1,8 @@
 // Now consider both if/else blocks together (keeping the added console.log lines). 
 // Run the code and examine the output. 
 
-// Given the values for fuelLevel, crewStatus and computerStatus, should launchReady be true or false?
-// Is the program behaving as expected?
+// Given the values for fuelLevel, crewStatus and computerStatus, should launchReady be true or false? - Given the conditions of the if statement and the output it should return a true. 
+// Is the program behaving as expected? - as both if statments are meeting their condition and returning the appropriate true output yes it is as expected so far. 
 
 let launchReady = false;
 let fuelLevel = 17000;

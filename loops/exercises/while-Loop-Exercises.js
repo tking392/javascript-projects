822860ace9720590a977767b56ab4e303c9d8510
Travis <azai392@gmail.com>
2024-01-15 23:16:10 -0600

    while loop working

--- loops/exercises/while-Loop-Exercises.js
+++ loops/exercises/while-Loop-Exercises.js
@@ -1,11 +1,15 @@
+const input = require('readline-sync');
+
 //Define three variables for the LaunchCode shuttle - one for the starting fuel level, another for the number of astronauts aboard, and the third for the altitude the shuttle reaches.
 
-
-
-
+let startingFuelLevel = input.question("Please input starting fuel level: ");
+let numberofAstronauts = 0;
+let shuttleAltitude = 0;
 
 /*Exercise #4: Construct while loops to do the following:
   a. Query the user for the starting fuel level. Validate that the user enters a positive, integer value greater than 5000 but less than 30000. */
+
+ 
 
 
 

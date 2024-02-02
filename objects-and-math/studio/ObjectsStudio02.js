ae8b155cbae38f270d77ee4ba1e299c82437092d
Travis <azai392@gmail.com>
2024-02-01 20:30:59 -0600

    studio part 2 attempted

--- objects-and-math/studio/ObjectsStudio02.js
+++ objects-and-math/studio/ObjectsStudio02.js
@@ -1,13 +1,56 @@
 // Code your orbitCircumference function here:
+function orbitCir (r) {
+  return Math.round(2*Math.PI*r);
+}
 
+console.log(orbitCir(10));
 
 // Code your missionDuration function here:
 
+function missionDuration (numOrb, orbRadius = 2000, orbSpd = 28000) {
+  let d = numOrb * orbitCir(orbRadius);
+  let t = d/orbSpd;
+  t = Math.round(t*100)/100;
+  console.log(`The mission will travel ${d}km around the planet, and it will take ${t} hours to complete.`)
+  return t;
+  
+}
 
+console.log(missionDuration(5,2000,28000));
 // Copy/paste your selectRandomEntry function here:
+//  let newIdNumbers = [];
 
+// function selectRandomEntry (arr) {
+    
+//     let index = Math.floor(Math.random() * arr.length);
+//     newIdNumbers.push(arr[index]);
+//     return arr.splice(index,1);
+// }
+
+function randomSelection(arr)
+ {
+   let index = Math.floor(Math.random()*arr.length);
+   return arr[index];
+ }
 
 // Code your oxygenExpended function here:
+
+// function oxygenExpended (indexCrewArr, newRadius, newSpd) {
+//   let t = missionDuration (3, newRadius, newSpd);
+//   let oTwo = Math.round(crew[indexCrewArr].o2Used(t)*1000)/1000;
+//   return console.log(`${crew[indexCrewArr].name} will perform the spacewalk, which will last ${t} hours and require ${oTwo}kg of oxygen.`);
+// }
+
+function oxygenExpended(candiateObj)
+{
+  let noOFOrbits=3;
+  let duration = missionDuration(noOFOrbits);
+  //let duration = missionDuration(noOFOrbits, 1500, 25000);
+  let oxygenused = Math.round(candiateObj.o2Used(duration)*1000)/1000;
+  //console.log(${candiateObj.name} will perform the spacewalk, which will last ${duration} hours and require ${oxygenused} kg of oxygen.);
+  return oxygenused;
+}
+
 
 
 // Candidate data & crew array.
@@ -55,4 +98,10 @@ let candidateA = {
  };
  
  let crew = [candidateA,candidateC,candidateE];
+
+
+randomCandidate = randomSelection(crew);
+console.log(oxygenExpended(randomCandidate));
+// console.log(oxygenExpended(2,8000,40000));
+
  

fb789feb846975d75fbf0a74b2e5af7a3de9bd36
Travis <azai392@gmail.com>
2024-02-12 17:08:10 -0600

    part 2 done!

--- exceptions/exercises/test-student-labs.js
+++ exceptions/exercises/test-student-labs.js
@@ -1,10 +1,14 @@
 function gradeLabs(labs) {
-  for (let i=0; i < labs.length; i++) {
+  try {
+    for (let i=0; i < labs.length; i++) {
     let lab = labs[i];
     let result = lab.runLab(3);
     console.log(`${lab.student} code worked: ${result === 27}`);
+    } } catch(err) {
+      console.log('Error Thrown!');
+    }
+ 
   }
-}
 
 let studentLabs = [
   {
@@ -22,3 +26,26 @@ let studentLabs = [
 ];
 
 gradeLabs(studentLabs);
+
+let studentLabs2 = [
+  {
+      student: 'Blake',
+      myCode: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Jessica',
+      runLab: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Mya',
+      runLab: function (num) {
+        return num * num;
+      }
+  }
+];
+
+gradeLabs(studentLabs2);

ccba10408451945e92b298344c47c41c3f7b8e58
Travis <azai392@gmail.com>
2024-02-19 17:01:48 -0600

    HTML Exercise done!!

--- classes/chapter-examples/ClassExamples01.js
+++ classes/chapter-examples/ClassExamples01.js
@@ -7,15 +7,28 @@ class Astronaut {
    }
 }
 
+class Car {
+  constructor(make, model, year, color, mpg){
+     this.make = make;
+     this.model = model;
+     this.year = year;
+     this.color = color;
+     this.mpg = mpg;
+  }
+}
+
 let fox = new Astronaut('Fox', 7, 12);
 
-console.log(fox);
-console.log(fox.age, fox.color);
+// console.log(fox);
+// console.log(fox.age, fox.color);
 
-fox.age = 9;
-fox.color = 'red';
+// fox.age = 9;
+// fox.color = 'red';
 
-console.log(fox);
-console.log(fox.age, fox.color);
+// console.log(fox);
+// console.log(fox.age, fox.color);
 
+
+let myCar = new Car('Tesla', 'Model S', 2019);
+console.log(myCar);
 //Try modifying or adding properties below.
--- exceptions/exercises/test-student-labs.js
+++ exceptions/exercises/test-student-labs.js
@@ -1,26 +1,27 @@
 function gradeLabs(labs) {
   try {
-    for (let i=0; i < labs.length; i++) {
-    let lab = labs[i];
-    let result = lab.runLab(3);
-    console.log(`${lab.student} code worked: ${result === 27}`);
-    } } catch(err) {
-      console.log('Error Thrown!');
+    for (let i = 0; i < labs.length; i++) {
+      let lab = labs[i];
+      let result = lab.runLab(3);
+      console.log(`${lab.student} code worked: ${result === 27}`);
     }
- 
+  } catch (err) {
+    console.log('Error Thrown!');
   }
+
+}
 
 let studentLabs = [
   {
     student: 'Carly',
     runLab: function (num) {
-        return Math.pow(num, num);
+      return Math.pow(num, num);
     }
   },
   {
     student: 'Erica',
     runLab: function (num) {
-        return num * num;
+      return num * num;
     }
   }
 ];
@@ -29,22 +30,22 @@ gradeLabs(studentLabs);
 
 let studentLabs2 = [
   {
-      student: 'Blake',
-      myCode: function (num) {
-        return Math.pow(num, num);
-      }
+    student: 'Blake',
+    myCode: function (num) {
+      return Math.pow(num, num);
+    }
   },
   {
-      student: 'Jessica',
-      runLab: function (num) {
-        return Math.pow(num, num);
-      }
+    student: 'Jessica',
+    runLab: function (num) {
+      return Math.pow(num, num);
+    }
   },
   {
-      student: 'Mya',
-      runLab: function (num) {
-        return num * num;
-      }
+    student: 'Mya',
+    runLab: function (num) {
+      return num * num;
+    }
   }
 ];
 

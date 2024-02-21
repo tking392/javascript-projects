8c2694cbede10271d6c8e92f9496ccc1868d15a8
Travis <azai392@gmail.com>
2024-02-20 23:17:57 -0600

    Part 3 Done!

--- more-on-functions/studio/part-three-number-sorting-easy-way.js
+++ more-on-functions/studio/part-three-number-sorting-easy-way.js
@@ -5,4 +5,33 @@ let nums3 = [200, 5, 4, 10, 8, 5, -3.3, 4.4, 0];
 
 //Sort each array in ascending order.
 
+function sortNumsAsc(nums) {
+    return nums.sort(function(a, b){return a - b});
+    
+}
+
+// Tests
+console.log(`
+
+     Ascending Order Tests
+-------------------------------
+Nums1 = ${sortNumsAsc(nums1)}
+Nums2 = ${sortNumsAsc(nums2)}
+Nums3 = ${sortNumsAsc(nums3)}
+-------------------------------`)
 //Sort each array in decending order.
+
+function sortNumsDec(nums) {
+    return nums.sort(function(a, b){return b - a});
+}
+
+// Tests
+
+console.log(`
+
+     Decending Order Tests
+-------------------------------
+Nums1 = ${sortNumsDec(nums1)}
+Nums2 = ${sortNumsDec(nums2)}
+Nums3 = ${sortNumsDec(nums3)}
+-------------------------------`)

1834b0aec60b8c30cedd75d6ecbca339092db003
Travis <azai392@gmail.com>
2024-02-20 23:26:03 -0600

    All Done

--- more-on-functions/studio/part-two-create-sorted-array.js
+++ more-on-functions/studio/part-two-create-sorted-array.js
@@ -32,9 +32,12 @@ function addRemoveMinVal(nums) {
 /* BONUS MISSION: Refactor your sorting function to use recursion below:
  */
 
+
+
 //Sample arrays for testing:
 let nums1 = [5, 10, 2, 42];
 let nums2 = [-2, 0, -10, -44, 5, 3, 0, 3];
 let nums3 = [200, 5, 4, 10, 8, 5, -3.3, 4.4, 0];
+
 
 console.log(addRemoveMinVal(nums2));

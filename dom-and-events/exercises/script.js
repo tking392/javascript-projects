747a4526d7bcb5df892faf2ed780faea62c9d470
Travis <azai392@gmail.com>
2024-03-04 16:02:34 -0600

    exercises done

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -4,12 +4,21 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
-
     button.addEventListener('click', event => {
         paragraph.innerHTML = 'Houston! We have liftoff!';
     });
-
+    missionAbort.addEventListener("mouseover", function( event ) {
+        event.target.style.backgroundColor = "red";
+     });
+    missionAbort.addEventListener("mouseout", function( event ) {
+        event.target.style.backgroundColor = "";
+     });
+     missionAbort.addEventListener('click', event => {
+        window.confirm("Are you sure you want to abort the mission?")
+        paragraph.innerHTML = 'Mission aborted! Space shuttle returning home.';
+    });
     
 }
+
 
 window.addEventListener("load", init);

ce3e6fe2066adc0aa7961d72270aa84400f9a628
Travis <azai392@gmail.com>
2024-03-04 20:00:38 -0600

    studio in progress

--- fetch/studio/script.js
+++ fetch/studio/script.js
@@ -1,1 +1,20 @@
 //TODO: Add Your Code Below
+window.addEventListener("load", function () {
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function (res) {
+        res.json().then(function (jsonRes) {
+            for (let i = 0; i < jsonRes.length; i++) {
+                const div = document.getElementById('container');
+                div.innerHTML += ` 
+            <ul>    
+                <li>ID: ${jsonRes[i].id}</li>
+                <li>Active: ${jsonRes[i].active}</li>
+                <li>Name: ${jsonRes[i].lastName}, ${jsonRes[i].firstName}</li>
+                <li>Skills: [${jsonRes[i].skills}]</li>
+                <li>Hours in Space: ${jsonRes[i].hoursInSpace} hrs</li>
+                <img src='${jsonRes[i].picture}'></img>
+            </ul>    
+            `
+            }
+        });
+    });
+}); 
